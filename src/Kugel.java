ee67c08ee1bacf0f92649f52128545fdca9e3860
tim.asmus <timasmus1@gmail.com>
2023-05-26 09:27:28 +0200

    i8 i

--- src/Kugel.java
+++ src/Kugel.java
@@ -6,6 +6,7 @@ import javax.media.opengl.GL;
 public class Kugel{
 
     public GLKugel  kugel;
+    public GLTafel tafel, tafel2;
     private Box dieBox;
     private Spielfeld feld;
 
@@ -16,32 +17,41 @@ public class Kugel{
     private Kugelfangen kugelfangen;
     double q = Math.random()*2.5-Math.random()*2.5;
     double w = Math.random()*2.5-Math.random()*2.5;
-
+    double u = 3;
     public Kugel(Box pBox) {
         kugel = new GLKugel(0,39,0,19);
         dieBox = pBox;
+
     }
     public void randomBewege(){
         vX = q;
         vZ = w;
 
 
-        if(this.getroffen()){kugel.setzeSichtbarkeit(false);}
-        if(kugel.gibZ()<=(-425)||
-                kugel.gibZ()>=425||
-                kugel.gibX()<=(-425)||
-                kugel.gibX()>=425) {
+        if(this.getroffen()){kugel.setzeSichtbarkeit(false);
+       u = u-1;
+
 
         }
-        else {
-            kugel.verschiebe(vX,0,vZ);}
-            if (kugel.gibX()  <= -425 || kugel.gibX()  >= 425) {
+        if(u==0){
+
+            tafel2 = new GLTafel(200,100,0,400,200);
+            tafel2.setzeText("Gewonnen!",10);}
+
+
+
+
+
+
+
+            kugel.verschiebe(vX,0,vZ);
+            if (kugel.gibX()  <= -400 || kugel.gibX()  >= 400||kugel.gibZ()  <= -400 || kugel.gibZ()  >= 400) {
                 vX = -vX;
-            }
-            if (kugel.gibZ()  <= -425 || kugel.gibZ()  >= 425) {
+
+
                 vZ = -vZ;
             }
-
+        kugel.verschiebe(vX,0,vZ);
 
 
 
